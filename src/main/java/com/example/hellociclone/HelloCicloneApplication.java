88f388105a81e163b0d57bbca5edf7e925b6119f
Mitsuyuki Shiiba <bufferings@gmail.com>
2022-08-05 11:49:34 +0900

    Add spring boot project

---
+++ src/main/java/com/example/hellociclone/HelloCicloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellociclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloCicloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloCicloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hellociclone/HelloCicloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellociclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloCicloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
